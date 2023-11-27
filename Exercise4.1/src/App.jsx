561f5a83152a41a337f88d005960bfa21955a0ba
JihadMuna <jojo.muna.2019@gmail.com>
2023-11-27 22:44:08 +0200

    Add Exercise4.1

--- Exercise4.1/src/App.jsx
+++ Exercise4.1/src/App.jsx
@@ -1,11 +1,42 @@
-import React from 'react'
-import Box1 from './box1/Box1'
+import React from 'react';
+import './App.css';
+
+export function Box1() {
+  return (
+    <div className='box1'>
+      <Box2 />
+    </div>
+  );
+}
+
+export function Box2() {
+  return (
+    <div className='box2'>
+      <Box3 />
+    </div>
+  );
+}
+
+export function Box3() {
+  return (
+    <div className='box3'>
+      <Box4 />
+      <Box4 />
+    </div>
+  );
+}
+
+export function Box4() {
+  return (
+    <div className='box4'>
+    </div>
+  );
+}
 
 export default function App() {
   return (
     <>
-      hello
       <Box1 />
     </>
-  )
+  );
 }
--- Exercise4.1/src/box4/Box4.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function () {
-  return (
-    <div style={{width: '50px', height: '50px,', backgroundColor: 'pirple'}}>
-      
-    </div>
-  )
-}
